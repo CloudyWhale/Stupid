af97a4e3dfb293fa1ad7a65d9d93f3357f44d81d
CloudyWhale <83542885+CloudyWhale@users.noreply.github.com>
2021-11-28 20:08:07 -0500

    Add a nice godot script

---
+++ sin float.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+
+# Declare member variables here.
+var sincount = 0
+export var sin_strength = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_wiggle()
+	pass
+
+func _wiggle():
+	if  sincount == 360:
+		sincount = 0
+		pass
+	sincount += 0.25
+	position.y = (sin(sincount) * sin_strength) + 350
